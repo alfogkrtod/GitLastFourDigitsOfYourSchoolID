79b65634021b926a5fa398a199de9214dbb66e5e
alfogkrtod <alfpgkrtod@naver.com>
2025-04-10 05:26:19 +0000

    make new brench and edit mynumber.c

--- mynumber.c
+++ mynumber.c
@@ -2,15 +2,24 @@
 
 int main() {
 	for(int i=0; i<=100; i++) {
-		if(i%2==1) {
-			printf("%d\n", i);
-		}
+		if(i%2==1) printf("%d\n", i);
 	}
 
+	for(int i=0; i<=100; i++) { 
+		if(i%2==0) printf("%d\n", i);
+	}
 	for(int i=0; i<=100; i++) {
-		if(i%2==0) {
-			printf("%d\n", i);
-		}
+		if(i==2) printf("%d\n", i); 
+		else if (i>2) {	
+			int flag = 1;
+			for(int k=2; k<i; k++) {
+				if(i%k==0) { 
+					flag=0;
+					break;
+				}
+			}
+			if(flag==1) printf("%d\n", i);
+		}	
 	}
 	return 0;
 }
